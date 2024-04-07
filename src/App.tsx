27821153ba266019968c02d12d5653c10386aa93
Andrew Gerez <andrewgerez18@gmail.com>
2024-04-07 13:53:15 -0300

    feat: create a handler mutation with canvas

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,8 @@ import { Canvas } from './canvas/canvas'
 
 export default function App() {
   return (
-    <Canvas />
+    <div className="canvas-container">
+      <Canvas args={{ width: 800, height: 500 }} />
+    </div>
   )
 }
--- src/canvas/canvas.tsx
+++ src/canvas/canvas.tsx
@@ -1,8 +1,9 @@
+import { useEffect } from 'react'
 import { CanvasHandler, CanvasProps } from './types'
 import { useCanvas } from './use-canvas'
 
 interface CanvasComponentProps {
-  readonly args: CanvasProps
+  readonly args?: CanvasProps
 }
 
 export function Canvas({ args }: CanvasComponentProps) {
@@ -12,5 +13,33 @@ export function Canvas({ args }: CanvasComponentProps) {
 
   const canvasRef = useCanvas(canvasDrawHandler)
 
-  return <canvas ref={canvasRef} {...args} />
+  function drawMutation(
+    context: CanvasRenderingContext2D | null | undefined,
+    count: number
+  ) {
+    context?.clearRect(0, 0, context.canvas.width, context.canvas.height)
+    const delta = count % 800
+    context?.fillRect(10 + delta, 10, 100, 100)
+  }
+
+  function handleMutation() {
+    const canvas = canvasRef.current
+    const context = canvas?.getContext('2d')
+    let count = 0
+    let animationID: number
+
+    const rerender = () => {
+      count ++
+      drawMutation(context, count)
+      animationID = window.requestAnimationFrame(rerender)
+    }
+
+    rerender()
+
+    return () => {
+      window.cancelAnimationFrame(animationID)
+    }
+  }
+
+  return <canvas ref={canvasRef} onClick={handleMutation} {...args} />
 }
