45c236810d3d5f6cc9807e97033f9124c0de73bb
Andrew Gerez <andrewgerez18@gmail.com>
2024-04-10 00:02:20 -0300

    feat: create env service

--- src/env/index.ts
+++ src/env/index.ts
@@ -1,6 +1,9 @@
 import { z } from 'zod'
 
-const envSchema = z.object({})
+const envSchema = z.object({
+  REACT_APP_API_URL: z.string().url(),
+  REACT_APP_API_KEY: z.string().uuid(),
+})
 
 const _env = envSchema.safeParse(process.env)
 
